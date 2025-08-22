ec0388098aca1102f2ef74f380d0c4e715023b0b
DongilMin <tasabi0826@gmail.com>
2025-08-23 00:24:06 +0900

    Add Spire config

--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/segmentio/kafka-go v0.4.48
-	github.com/spiffe/go-spiffe/v2 v2.5.0
+	github.com/spiffe/go-spiffe/v2 v2.1.7
 	go.uber.org/zap v1.27.0
 )
 
@@ -40,6 +40,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- pkg/tls/tls.go
+++ pkg/tls/tls.go
@@ -1,73 +1,83 @@
 package tls
 
 import (
+    "context"
     "crypto/tls"
-    "crypto/x509"
     "fmt"
-    "os"
     "time"
     
+    "github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+    "github.com/spiffe/go-spiffe/v2/workloadapi"
     "go.uber.org/zap"
 )
 
 type TLSConfig struct {
     Enabled      bool   `envconfig:"TLS_ENABLED" default:"false"`
-    CertPath     string `envconfig:"TLS_CERT_PATH" default:"/run/spire/certs/cert.pem"`
-    KeyPath      string `envconfig:"TLS_KEY_PATH" default:"/run/spire/certs/key.pem"`
-    CAPath       string `envconfig:"TLS_CA_PATH" default:"/run/spire/certs/ca.pem"`
-    ClientAuth   bool   `envconfig:"TLS_CLIENT_AUTH" default:"false"`
+    SocketPath   string `envconfig:"SPIRE_SOCKET_PATH" default:"unix:///run/spire/sockets/agent.sock"`
 }
+
+var x509Source *workloadapi.X509Source
 
 func LoadTLSConfig(cfg *TLSConfig, logger *zap.Logger) (*tls.Config, error) {
     if !cfg.Enabled {
+        logger.Info("TLS is disabled")
         return nil, nil
     }
     
-    cert, err := tls.LoadX509KeyPair(cfg.CertPath, cfg.KeyPath)
+    ctx := context.Background()
+    
+    // SPIRE Workload API를 통해 X509 소스 생성
+    source, err := workloadapi.NewX509Source(
+        ctx,
+        workloadapi.WithClientOptions(
+            workloadapi.WithAddr(cfg.SocketPath),
+        ),
+    )
     if err != nil {
-        return nil, fmt.Errorf("failed to load cert/key: %w", err)
+        return nil, fmt.Errorf("unable to create X509Source: %w", err)
     }
     
-    tlsConfig := &tls.Config{
-        Certificates: []tls.Certificate{cert},
-        MinVersion:   tls.VersionTLS12,
-    }
+    x509Source = source
     
-    if cfg.ClientAuth && cfg.CAPath != "" {
-        caCert, err := os.ReadFile(cfg.CAPath)
-        if err != nil {
-            return nil, fmt.Errorf("failed to read CA cert: %w", err)
-        }
-        
-        caCertPool := x509.NewCertPool()
-        caCertPool.AppendCertsFromPEM(caCert)
-        
-        tlsConfig.ClientCAs = caCertPool
-        tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
-    }
+    // mTLS 서버 설정 생성
+    tlsConfig := tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny())
+    tlsConfig.MinVersion = tls.VersionTLS12
     
-    logger.Info("TLS configuration loaded",
-        zap.String("cert_path", cfg.CertPath),
-        zap.Bool("client_auth", cfg.ClientAuth))
+    logger.Info("SPIRE TLS configuration loaded",
+        zap.String("socket_path", cfg.SocketPath),
+        zap.Bool("mtls_enabled", true))
     
     return tlsConfig, nil
 }
 
 func WatchCertificates(cfg *TLSConfig, reloadFunc func(*tls.Config) error, logger *zap.Logger) {
+    if x509Source == nil {
+        logger.Error("X509Source is not initialized")
+        return
+    }
+    
     ticker := time.NewTicker(30 * time.Second)
     defer ticker.Stop()
     
     for range ticker.C {
-        newTLSConfig, err := LoadTLSConfig(cfg, logger)
+        // SPIRE가 자동으로 인증서를 갱신하므로 별도 처리 불필요
+        // 하지만 로깅을 위해 상태 체크
+        svid, err := x509Source.GetX509SVID()
         if err != nil {
-            logger.Error("Failed to reload TLS config", zap.Error(err))
+            logger.Error("Failed to get X509 SVID", zap.Error(err))
             continue
         }
         
-        if err := reloadFunc(newTLSConfig); err != nil {
-            logger.Error("Failed to apply new TLS config", zap.Error(err))
-        } else {
-            logger.Info("TLS certificates reloaded successfully")
-        }
+        logger.Info("Certificate status",
+            zap.String("spiffe_id", svid.ID.String()),
+            zap.Time("expiry", svid.Certificates[0].NotAfter),
+            zap.Duration("ttl", time.Until(svid.Certificates[0].NotAfter)))
+    }
+}
+
+// Cleanup 함수 추가
+func Cleanup() {
+    if x509Source != nil {
+        x509Source.Close()
     }
 }
